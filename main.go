ff0ba013c5d3b7ec5d2305ee7a64594bccc27471
yude <yu@yude.moe>
2020-08-27 15:24:07 +0900

    chisel のダウンロード元を変更

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bufio"
+	"compress/gzip"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"runtime"
 	"time"
 
 	"fyne.io/fyne"
@@ -17,23 +19,43 @@ import (
 )
 
 func main() {
+	// 環境を設定
 	os.Setenv("FYNE_FONT", "C:\\Windows\\Fonts\\meiryo.ttc")
 	exec.Command("chcp 65001")
+	// GUI を設定
 	a := app.New()
 	w := a.NewWindow("chisel - A fast TCP tunnel over HTTP")
+
 	hello, desc := widget.NewLabelWithStyle("Minecraft 接続用プロキシ", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}), widget.NewLabelWithStyle("chisel.exe が存在しているか確かめます。", fyne.TextAlignCenter, fyne.TextStyle{})
 	var launch *widget.Button
 	logField := widget.NewMultiLineEntry()
+	// 実行ログ関係
 	logField.SetPlaceHolder("ここに実行ログが表示されます。")
 	logField.Disable()
+	// ボタンの動画
 	launch = widget.NewButton("起動する", func() {
+		// chisel のダウンロード元をOSによって分岐
+		var url string
+		if runtime.GOOS == "windows" {
+			url = "https://github.com/jpillora/chisel/releases/download/v1.6.0/chisel_1.6.0_windows_amd64.gz"
+		}
+		if runtime.GOOS == "darwin" {
+			url = "https://github.com/jpillora/chisel/releases/download/v1.6.0/chisel_1.6.0_darwin_amd64.gz"
+		}
+		var newfilename string
+		if runtime.GOOS == "windows" {
+			newfilename = "chisel.exe"
+		}
+		if runtime.GOOS == "darwin" {
+			newfilename = "chisel"
+		}
+		// ボタンの状態を変更
 		launch.SetText("起動済み")
 		launch.Disable()
-
-		if f, err := os.Stat("chisel_x64.exe"); os.IsNotExist(err) || f.IsDir() {
+		// chisel をダウンロードして展開する
+		if f, err := os.Stat(newfilename); os.IsNotExist(err) || f.IsDir() {
 			desc.SetText("chisel が存在しません。ダウンロードします。")
-			url := "https://chiselmirror.now.sh/chisel_x64.exe"
-			output, err := os.Create("chisel")
+			output, err := os.Create("chisel.gz")
 			defer output.Close()
 
 			response, err := http.Get(url)
@@ -44,18 +66,58 @@ func main() {
 			}
 			defer response.Body.Close()
 			n, err := io.Copy(output, response.Body)
-			desc.SetText(fmt.Sprintf("%d バイトをダウンロードしました。", n))
+			desc.SetText(fmt.Sprintf("%d バイトをダウンロードしました。展開します。", n))
 			time.Sleep(1 * time.Second)
-			desc.SetText("プロキシを起動します。")
+			// 展開部
+			desc.SetText("展開しています...")
+			filename := "chisel.gz"
+			gzipfile, errdec := os.Open(filename)
+
+			if err != nil {
+				logField.SetText(fmt.Sprintln(errdec))
+				println(errdec)
+				//os.Exit(1)
+			}
+
+			reader, err := gzip.NewReader(gzipfile)
+			if err != nil {
+				logField.SetText(fmt.Sprintln(errdec))
+				println(errdec)
+				os.Exit(1)
+			}
+			defer reader.Close()
+
+			writer, err := os.Create(newfilename)
+
+			if err != nil {
+				logField.SetText(fmt.Sprintln(errdec))
+				println(errdec)
+				os.Exit(1)
+				defer writer.Close()
+			}
+
+			defer writer.Close()
+			if _, err = io.Copy(writer, reader); err != nil {
+				logField.SetText(fmt.Sprintln(errdec))
+				println(errdec)
+				os.Exit(1)
+				defer writer.Close()
+			}
+			defer writer.Close()
+			defer gzipfile.Close()
+			if err := os.Remove("chisel.gz"); err != nil {
+				fmt.Println(err)
+			}
+			desc.SetText("展開しました。プロキシを起動します。")
 		} else {
 			desc.SetText("chisel が既に存在します。プロキシを起動します。")
 		}
-
-		c := "chisel_x64.exe"
+		time.Sleep(1 * time.Second)
+		// chisel をバックグラウンドで起動
+		c := "chisel.exe"
 		p := []string{"client", "proxy.yude.moe", "25565"}
 		cmd := exec.Command(c, p...)
 		cmd.Dir = "."
-
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
 			log.Fatal(err)
