f42c31de6c828911e184dceb750a7d0951b20cd6
yude <yu@yude.moe>
2020-08-27 09:15:22 +0900

    Make button disabled when clicked

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"fyne.io/fyne"
 
@@ -16,8 +17,12 @@ func main() {
 	os.Setenv("FYNE_FONT", "C:\\Windows\\Fonts\\meiryo.ttc")
 	a := app.New()
 	w := a.NewWindow("chisel - A fast TCP tunnel over HTTP")
-	hello, desc := widget.NewLabelWithStyle("こんにちは！", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}), widget.NewLabelWithStyle("chisel.exe が存在しているか確かめます。", fyne.TextAlignCenter, fyne.TextStyle{})
-	launch := widget.NewButton("起動する", func() {
+	hello, desc := widget.NewLabelWithStyle("Minecraft 接続用プロキシ", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}), widget.NewLabelWithStyle("chisel.exe が存在しているか確かめます。", fyne.TextAlignCenter, fyne.TextStyle{})
+	var launch *widget.Button
+	//var log *widget.
+	launch = widget.NewButton("起動する", func() {
+		launch.SetText("起動済み")
+		launch.Disable()
 		if f, err := os.Stat("chisel.exe"); os.IsNotExist(err) || f.IsDir() {
 			desc.SetText("chisel.exe が存在しません。ダウンロードします。")
 			url := "https://chiselmirror.now.sh/chisel.exe"
@@ -33,9 +38,16 @@ func main() {
 			defer response.Body.Close()
 			n, err := io.Copy(output, response.Body)
 			desc.SetText(fmt.Sprintf("%d バイトをダウンロードしました。", n))
+			time.Sleep(1 * time.Second)
+			desc.SetText("プロキシを起動します。")
 		} else {
-			desc.SetText("chisel.exe が存在します。プロキシを起動します。")
+			desc.SetText("chisel.exe が既に存在します。プロキシを起動します。")
 		}
+		/* w.SetContent(widget.NewVBox(
+			hello,
+			desc,
+			log,
+		))*/
 	})
 	w.SetFixedSize(true)
 	w.Resize(fyne.NewSize(390, 100))
